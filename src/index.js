7ebd2a67e2c23bb54ee127d291c4b630ee7b076c
dzinrai <dzinrai@mail.ru>
2020-03-05 23:29:50 +0300

    feat: implement morse

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let arr = [];
+  for(let i = 0; i<expr.length / 10; i++){
+    const dec_symb = expr.slice(i*10,i*10+10);  // slicing string by 10 elements and then proceed with it
+    if(/[*]{10}/.test(dec_symb)){ 
+        // *********** count as whitespace
+      arr.push(" ");
+    }else{
+      const array_of_doubles = []; //  [ "00", "00", "10", "11", "10" ]
+      arr.push("");
+      for(let k = 0; k<dec_symb.length/2; k++){
+        array_of_doubles.push(dec_symb.slice(k*2,k*2+2));
+      }
+      for(let j of array_of_doubles){
+        //  in array [ "00", "00", "10", "11", "10" ]
+        //  10 = '.' and 11 = '-'
+        if( /^10$/.test(j) ){
+          arr[i]+='.';
+        }else if(/^11$/.test(j)){
+          arr[i]+='-';
+        }
+      }
+    }
+    // after convert sliced string of 10 elements '0000111010'=>'-..'
+    // take letter or number from <MORSE_TABLE> (keys === exact morse code) except spaces
+    arr[i] = /\s/.test(arr[i]) ? " " : MORSE_TABLE[arr[i]]; 
+  }
+  return arr.join('');
 }
 
 module.exports = {
